aa7d510bba695b9b37c3f08a5003d8a7278083d1
seovain <r00251105@mymtu.ie>
2025-10-09 10:19:46 +0100

    modified:   models/submission.js 	modified:   models/user.js

--- models/submission.js
+++ models/submission.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose'); //imports the schema
 
 const submissionSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    url: { type: String, required: true },
-    content: { type: String, required: true },
-    author:  { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    createdAt: { type: Date, default: Date.now }
+    title: { type: String, required: true }, //title of the submission
+    url: { type: String, required: true }, //URL of the submission
+    content: { type: String, required: true }, //content or description of the submission
+    author:  { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, //reference to the User model
+    createdAt: { type: Date, default: Date.now } //timestamp
 });
 
 const Submission = mongoose.model('Submission', submissionSchema);
--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose'); //imports the schema
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true }, //username for the user
     password: { type: String, required: true }, //hashed password
-    email: { type: String, required: true, unique: true },
-    createdAt: { type: Date, default: Date.now }
+    email: { type: String, required: true, unique: true }, //user email
+    createdAt: { type: Date, default: Date.now } //timestamp
 });
 
 const User = mongoose.model('User', userSchema);
